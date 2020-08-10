4d76c52fd4d8d207f9b9039883b736cb92d29d62
Zeeshan Ahmad <zeeshan.official@gmail.com>
2020-08-10 23:23:21 +0530

    Added 2 more problem solutions
    Description:
    1. Function to delete the nth node from the end of the given linked list
    2. Merge two sorted linked lists and return it as a new sorted list.

--- LinkedList.js
+++ LinkedList.js
@@ -82,8 +82,56 @@ LinkedList.prototype.deleteAllKey = function(key) {
   }
 }
 
-let ll = new LinkedList();
-ll.addAtFront(5);
+// Function to delete the nth node from the end of the given linked list
+
+LinkedList.prototype.deleteNthFromEnd = function(n) {
+  let fast = this.head;
+  let slow = this.head;
+  let count = 0;
+  let stop = false;
+  while(fast && count!=n) {
+    if(fast.next ==  null) {
+      if(count == n-1) {
+        this.head = this.head.next;
+        stop = true;
+      }
+    }
+    fast = fast.next;
+    count++;
+  }
+  while(fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+  if(!stop) {
+    slow.next = slow.next.next;
+  }
+  
+}
+
+// Merge two sorted linked lists and return it as a new sorted list.
+LinkedList.prototype.sortedMerge = function(l1, l2) {
+  let dummyNode = new Node(0, null);
+  let tail = dummyNode;
+  debugger;
+  while(l1 && l2) {
+    if(l1.value < l2.value) {
+      tail.next = l1;
+      tail = tail.next;
+      l1 = l1.next;
+    } else {
+      tail.next = l2;
+      tail = tail.next;
+      l2 = l2.next;
+    }
+  }
+  // the exhausted list will be null
+  tail.next = l1 || l2;
+  return dummyNode.next;
+}
+let l1 = new LinkedList();
+let l2 = new LinkedList();
+/*ll.addAtFront(5);
 ll.addAtFront(3);
 ll.addAfter(8, ll.head);
 //3 -> 8 -> 5
@@ -92,5 +140,15 @@ ll.addAtEnd(8)
 //3 -> 8 -> 5 -> 12 -> 8
 
 //ll.deleteKey(8); // 3 -> 5 -> 12 -> 8
-ll.deleteAllKey(8); // 3 -> 5 -> 12
-console.log(ll.head.next);
+ll.deleteAllKey(8); // 3 -> 5 -> 12*/
+
+l1.addAtEnd(1);
+l1.addAtEnd(2);
+l1.addAtEnd(4);
+l2.addAtEnd(1);
+l2.addAtEnd(3);
+l2.addAtEnd(4);
+
+let ll = new LinkedList();
+let newLl = ll.sortedMerge(l1.head, l2.head);
+console.log(newLl)
